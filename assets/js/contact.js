794ee0fed8cbcb355f129f145fc1755461e073f6
AtmosDerrick <93062640+AtmosDerrick@users.noreply.github.com>
2024-02-10 15:25:37 -0800

    done responsiveness

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -27,3 +27,7 @@ clickShowElements.forEach(function (element) {
     }
   });
 });
+
+document.querySelector(".menu-toggle").addEventListener("click", function () {
+  document.querySelector(".nav-links").classList.toggle("active");
+});

e3d7eb2f22d450c564b4aa7393b74d26f5f9b86c
AtmosDerrick <93062640+AtmosDerrick@users.noreply.github.com>
2024-02-09 16:30:47 -0800

    done working on the about

--- assets/js/about.js
+++ assets/js/about.js
@@ -8,3 +8,26 @@ function handleClick(element) {
   // Add active class to clicked div
   element.classList.add("active");
 }
+
+function showSection(sectionId, element) {
+  // Hide all divs except the one that was clicked
+  var divs = document.querySelectorAll("#sectionContainer div");
+  divs.forEach((div) => {
+    if (div !== element) {
+      div.classList.remove("active");
+    }
+  });
+
+  // Add 'active' class to the clicked div
+  element.classList.add("active");
+
+  // Show or hide the appropriate section
+  var sections = document.querySelectorAll(".section");
+  sections.forEach((section) => {
+    if (section.id === sectionId) {
+      section.style.display = "block";
+    } else {
+      section.style.display = "none";
+    }
+  });
+}

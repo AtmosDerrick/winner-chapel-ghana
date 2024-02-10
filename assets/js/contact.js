b581f2ee32a71a432a251a005dc107622e72990a
AtmosDerrick <93062640+AtmosDerrick@users.noreply.github.com>
2024-02-10 14:31:47 -0800

    done with all the pages

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,30 +1,29 @@
+// Get all elements with class "clickShow"
+var clickShowElements = document.querySelectorAll(".clickShow");
 
+// Loop through each element and add click event listener
+clickShowElements.forEach(function (element) {
+  element.addEventListener("click", function () {
+    // Hide all showPara elements and reset icon and color for all clickShow elements
+    clickShowElements.forEach(function (clickShowElement) {
+      clickShowElement.style.color = "inherit"; // Reset color
+      clickShowElement.querySelector("i").className = "fa fa-caret-up"; // Reset icon
+      var showPara = clickShowElement.nextElementSibling;
+      showPara.style.display = "none"; // Hide all showPara elements
+    });
 
-function showContent(divNumber) {
-    var content = "";
-
-    switch (divNumber) {
-        case 1:
-            content = "Content for Div 1";
-            break;
-        case 2:
-            content = "Content for Div 2";
-            break;
-        case 3:
-            content = "Content for Div 3";
-            break;
-        case 4:
-            content = "Content for Div 4";
-            break;
-        case 5:
-            content = "Content for Div 5";
-            break;
-        default:
-            content = "No content available";
+    // Toggle the visibility of the corresponding showPara element
+    var showPara = element.nextElementSibling;
+    if (showPara.style.display === "none" || showPara.style.display === "") {
+      showPara.style.display = "block";
+      // Change icon and its color upon click
+      element.querySelector("i").className = "fa fa-caret-down"; // Change icon
+      element.style.color = "red"; // Change color
+    } else {
+      showPara.style.display = "none";
+      // Change icon and its color back to default
+      element.querySelector("i").className = "fa fa-caret-up"; // Change icon
+      element.style.color = "inherit"; // Change color
     }
-
-    var contentArea = document.getElementById("contentArea");
-    contentArea.style.display = "block";
-    contentArea.innerHTML = content;
-}
-
+  });
+});
